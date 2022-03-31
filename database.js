01df128979eee7ce957345367e28204ba166c171
imR2D2 <arturo.hzb14@gmail.com>
2022-03-31 04:06:04 -0600

    Segundo

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
     host: "us-cdbr-east-05.cleardb.net",
-    user: "b134f4a3d03ac4",
-    password: "afb419ba",
+    user: "ba38a3eb245455",
+    password: "48601a4f",
     port: 3306,
-    database: "heroku_40ff34ee5194653",
+    database: "heroku_82676d270670adf",
     secret_key: "15fldsmdr15"
 };
 exports.config = config;
